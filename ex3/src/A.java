fb5b24efa7272916d205a7bf9dcaded69572ae2f
PauMasia <Dears2004pau@gmail.com>
2024-09-13 10:10:50 +0200

    V-0.2 13/9/2024

---
+++ ex3/src/A.java
@@ -0,0 +1,20 @@
+class Executable {
+    void executar(){}
+}
+
+class Executor {
+    void executarXVegades(int x, Executable executable){
+        for (int i = 0; i < x; i++) {
+            executable.executar();
+        }
+    }
+}
+
+public class A {
+    public static void main(String[] args) {
+
+        Executor executor = new Executor();
+
+        executor.executarXVegades(10, new Executable());
+    }
+}
