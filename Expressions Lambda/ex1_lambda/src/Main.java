1f58788ab198f43e2674cd3baa17ede2a572d68c
PauMasia <Dears2004pau@gmail.com>
2024-09-21 17:58:34 +0200

    V-0.8 21/9/2024

---
+++ Expressions Lambda/ex1_lambda/src/Main.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        new Main().start();
+    }
+
+    public void start() {
+
+        JButton button1 = new JButton("Mega Button1");
+        button1.setBounds(0,0, 400,200);
+        button1.addActionListener(e-> System.out.println("S'ha polsat el botó Mega Button1"));
+        add(button1);
+
+        JButton button2 = new JButton("Mega Button2");
+        button2.setBounds(0,200, 400,200);
+        ActionListener accion2= e -> System.out.println("S'ha polsat el botó Mega Button2");
+        button2.addActionListener(accion2);
+        add(button2);
+
+        JButton button3 = new JButton("Mega Button3");
+        button3.setBounds(0,400, 400,200);
+        button3.addActionListener();
+        add(button3);
+
+        setSize(600, 200);
+        setLayout(new GridLayout(1, 3));
+        setVisible(true);
+    }
+}
